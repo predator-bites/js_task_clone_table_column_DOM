0d81f56466a5b75a778557f8ccee6e113705a02c
DropPredator <ionchiccc@gmail.com>
2025-09-07 10:51:26 +0300

    ready

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,68 @@
 'use strict';
 
-// write your code here
+const tBody = document.querySelector('table tbody');
+const tFoot = document.querySelector('table tfoot');
+const tHead = document.querySelector('table thead');
+
+function getVertLine(tableBody, tableHead, tableFoot, lineIndex) {
+  if (
+    typeof tableBody !== 'object' ||
+    typeof tableHead !== 'object' ||
+    typeof tableFoot !== 'object' ||
+    typeof lineIndex !== 'number'
+  ) {
+    return null;
+  }
+
+  const rows = [...tableBody.rows];
+
+  const bodyElems = rows.map((tr) => {
+    const neededTd = tr.cells[lineIndex];
+
+    if (neededTd !== null || typeof neededTd !== 'undefined') {
+      return neededTd;
+    }
+
+    return null;
+  });
+
+  return [
+    tableHead.rows[0].cells[lineIndex],
+    bodyElems,
+    tableFoot.rows[0].cells[lineIndex],
+  ];
+}
+
+function pasteLine(
+  tableBody,
+  tableHead,
+  tableFoot,
+  [thHead, tdsBody, thFoot],
+  indexToPaste,
+) {
+  const bodyRows = [...tableBody.rows];
+  const headerRow = [...tableHead.rows][0];
+  const footerRow = [...tableFoot.rows][0];
+
+  const headerCellToMove = headerRow.cells[indexToPaste];
+  const footerCellToMove = footerRow.cells[indexToPaste];
+
+  headerCellToMove.insertAdjacentElement('beforebegin', thHead.cloneNode(true));
+  footerCellToMove.insertAdjacentElement('beforebegin', thFoot.cloneNode(true));
+
+  if (bodyRows.length === tdsBody.length) {
+    for (let i = 0; i < tdsBody.length; i++) {
+      const tdToMove = bodyRows[i].cells[indexToPaste];
+
+      if (typeof tdToMove !== 'object') {
+        continue;
+      }
+
+      tdToMove.insertAdjacentElement('beforebegin', tdsBody[i].cloneNode(true));
+    }
+  }
+}
+
+const line = getVertLine(tBody, tHead, tFoot, 1);
+
+pasteLine(tBody, tHead, tFoot, line, 4);
